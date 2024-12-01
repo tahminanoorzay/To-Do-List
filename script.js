81557c5d9382c978d3667654d82721f915508b20
TAHMINA NOORZAY <tahminanoorzay1380@gmail.com>
2024-12-01 21:32:37 +0430

    Add delete Btn

--- script.js
+++ script.js
@@ -2,16 +2,32 @@ let title = document.getElementById("title");
 let description = document.getElementById("description");
 let addBtn = document.getElementById("addBtn");
 let deleteBtn = document.getElementById("deleteBtn");
+let startTime = document.getElementById("startTime");
+let endTime = document.getElementById("endTime");
 
 addBtn.addEventListener("click", () => {
   let newTitle = document.createElement("div");
   let newDescription = document.createElement("div");
+  let newStartTime = document.createElement("div");
+  let newEndTime = document.createElement("div");
 
   newTitle.textContent = title.value;
   newDescription.textContent = description.value;
+  newStartTime.textContent = startTime.value;
+  newEndTime.textContent = endTime.value;
 
   document.body.appendChild(newTitle);
   newTitle.classList.add("newTitle");
   document.body.appendChild(newDescription);
   newDescription.classList.add("newDescription");
+  document.body.appendChild(newStartTime);
+  newStartTime.classList.add("newStartTime");
+  document.body.appendChild(newEndTime);
+  newEndTime.classList.add("newEndTime");
+});
+deleteBtn.addEventListener("click", () => {
+  title.value = "";
+  description.value = "";
+  startTime.value = "";
+  endTime.value = "";
 });
